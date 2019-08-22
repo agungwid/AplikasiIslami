959d2ad1a22b253fc767724dcaebbfdc02a7f47b
agungwid <12355wa@gmail.com>
2019-08-22 16:09:20 +0700

    Initial commit

---
+++ app/src/main/java/com/agungwid/aplikasiislami/Kota.kt
@@ -0,0 +1,9 @@
+package com.agungwid.aplikasiislami
+
+class Kota {
+    var id: Int? = null
+    var nama: String? = null
+    override fun toString(): String {
+        return nama.toString()
+    }
+}
